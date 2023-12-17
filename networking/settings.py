18b1b10f8e1daa7616f2ee453bb8dd25ec8108cd
Babala Assih <babal@macbook-pro.myfiosgateway.com>
2023-12-16 21:01:55 -0500

    after messages

--- networking/settings.py
+++ networking/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NETWORKING-B2B.settings')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -268,3 +269,4 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     },
 }
+
