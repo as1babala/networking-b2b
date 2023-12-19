cd62d431ff79b132a30368a3267eb72fe394b670
Babala Assih <babala.assih@gmail.com>
2023-12-19 18:54:28 -0500

    updated Loging and registration pages

---
+++ static/js/login.js
@@ -0,0 +1,5 @@
+// login.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any JavaScript you need for your login page here.
+    // For example, you can add form validations before submission.
+});
