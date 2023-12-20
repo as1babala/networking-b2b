8e9f1bf8e5d3a95593c056ac1a35f7ee8ea8f263
Babala Assih <babala.assih@gmail.com>
2023-12-20 15:34:26 -0500

    added history tracker

--- networking/settings.py
+++ networking/settings.py
@@ -233,7 +233,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = [os.path.join(BASE_DIR, "sent_emails")]
+#EMAIL_FILE_PATH = [os.path.join(BASE_DIR, "sent_emails")]
+#EMAIL_ROOT = '/sent_emails/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
